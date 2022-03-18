842995fd1ce37fe0609c0a348a9b89134ac9f7d0
zeserrado-marques <57963638+zeserrado-marques@users.noreply.github.com>
2022-03-18 10:50:26 +0000

    Learning Jython for Fiji

---
+++ jython_fiji/learning_the_basics.py
@@ -0,0 +1,26 @@
+# go to https://syn.mrc-lmb.cam.ac.uk/acardona/fiji-tutorial/ to follow along the tutorial
+
+from ij import IJ
+from ij.io import FileSaver
+import os
+
+# creates instance of imagePlus object of active image
+# imp points to the active image
+imp = IJ.getImage()
+
+# get image name
+img_name = imp.getTitle()
+# string slicing baby
+img_name = img_name[:img_name.find(".png")]
+
+
+# save the image
+# create a FileSaver instance with "imp" as its parameter 
+fs = FileSaver(imp)
+# call function save on the filesaver instance. to save the parameter it has within
+# it goes to the last directory used to save (or open? maybe the last acessed directory)
+#fs.save()
+
+# we can write the folder + filename where we want to save the image as a parameter for any of the save methods FileSaver has.
+save_folder = r"G:\zemarques\user_images\carolinaPereira" + os.sep + img_name + ".tif"
+fs.saveAsTiff(save_folder)
