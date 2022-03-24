a26e26f31488505e79ec0e110620cd938c00badc
zeserrado-marques <57963638+zeserrado-marques@users.noreply.github.com>
2022-03-24 17:50:51 +0000

    Update image_statistics.py

--- jython_fiji/image_statistics.py
+++ jython_fiji/image_statistics.py
@@ -1,5 +1,5 @@
 # statistics
-from ij import IJ  
+from ij import IJ
 from ij.process import ImageStatistics as IS
 
 # get imagePlus and ImageProcessor (here pixel data is stored)
@@ -7,9 +7,18 @@ imp = IJ.getImage()
 image_processor = imp.getProcessor()
 
 print IS.MEAN
-
+
 # bitwise operation OR to generate a certain int value. This value will correspond to which kind of statistics we want to retrieve from the IS.getStatistics method.
-options = IS.MEAN | IS.MEDIAN | IS.MIN_MAX
+options = IS.MEAN | IS.MEDIAN | IS.MIN_MAX | IS.STD_DEV
 stats = IS.getStatistics(image_processor, options, imp.getCalibration())
 
-print options, imp.title
+# print statistics on the image
+print "Image statistics for", imp.title
+print "Mean:", stats.mean
+print "Median:", stats.median
+print "standard deviation", stats.stdDev
+print "Min and max:", stats.min, "-", stats.max
+
+cal = imp.getCalibration()
+
+print cal.getValueUnit()
