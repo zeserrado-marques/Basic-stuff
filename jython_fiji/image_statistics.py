fb4355af50bb1b2f9e87395813ca53a4a54a68cd
zeserrado-marques <57963638+zeserrado-marques@users.noreply.github.com>
2022-03-24 10:43:25 +0000

    Update image_statistics.py

--- jython_fiji/image_statistics.py
+++ jython_fiji/image_statistics.py
@@ -7,7 +7,9 @@ imp = IJ.getImage()
 image_processor = imp.getProcessor()
 
 print IS.MEAN
+
+# bitwise operation OR to generate a certain int value. This value will correspond to which kind of statistics we want to retrieve from the IS.getStatistics method.
+options = IS.MEAN | IS.MEDIAN | IS.MIN_MAX
+stats = IS.getStatistics(image_processor, options, imp.getCalibration())
 
-
-options = IS.MEAN | IS.MEDIAN | IS.MIN_MAX  # Geo, eu não sei o que é isto. Ajuda-me :(
-stats = IS.getStatistics(ip, options, imp.getCalibration())
+print options, imp.title
