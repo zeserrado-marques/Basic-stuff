9c5dbcd9880b0aa4c1ae10565e6f8cf40bf7c82c
zeserrado-marques <57963638+zeserrado-marques@users.noreply.github.com>
2022-04-12 13:05:21 +0100

    Update get_mip_name_from_list.py
    Now with lambda

--- jython_fiji/get_mip_name_from_list.py
+++ jython_fiji/get_mip_name_from_list.py
@@ -1,9 +1,6 @@
 from ij import IJ
 from ij import WindowManager as WM
 
-def findMipName(str1, str2):
-	return str1 if str1.find("MAX_") > -1 else str2
-
 # get all images
 images = map(WM.getImage, WM.getIDList())
 
@@ -11,7 +8,7 @@ images = map(WM.getImage, WM.getIDList())
 lst_titles = map(str, WM.getImageTitles())
 
 # get image name with "MAX_"
-mip_name = reduce(findMipName, lst_titles)
+mip_name = reduce(lambda x, y: x if x.find("MAX_") > -1 else y, lst_titles)
 
 
 IJ.selectWindow(mip_name)
